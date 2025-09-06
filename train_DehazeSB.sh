2fecc98d93874dc766db4db4a5e5e8fbb1e6a6c7
ywxjm <yunweilan@mail.ustc.edu.cn>
2025-09-06 09:17:51 +0800

    Update train_DehazeSB.sh

--- train_DehazeSB.sh
+++ train_DehazeSB.sh
@@ -1,8 +1,8 @@
 ### /data/lanyunwei/my_experiment_results/unpaired_cycle_gan_dehazing_dataset_simple
 ### /data/lanyunwei/my_experiment_results/unpaired_cycle_gan_dehazing_dataset_new_v2_delete_bluesky
 
-python /code/train.py \
---dataroot /data/lanyunwei/my_experiment_results/unpaired_cycle_gan_dehazing_dataset_simple \
+python train.py \
+--dataroot /data/lanyunwei/my_experiment_results/datasey \
 --mode dehazesb --model dehazesb \
 --dataset_mode unpaired \
 --batch_size 4 \
